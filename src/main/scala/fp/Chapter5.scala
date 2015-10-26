722d33754c9d0ce50d86208d807942d28a6c86ab
Thomas Franquelin <ostapneko@gmail.com>
2015-10-26 09:03:09 +0000

    More streams

--- src/main/scala/fp/Chapter5.scala
+++ src/main/scala/fp/Chapter5.scala
@@ -1,42 +1,54 @@
 package fp
 
 object Chapter5 {
-  sealed trait StreamCustom[+A] {
+  sealed trait MyStream[+A] {
     def toList: List[A] = this match {
       case Empty => Nil
       case Cons(h, t) => h() :: t().toList
     }
 
-    def take(n: Int): StreamCustom[A] = this match {
-      case Empty => StreamCustom.empty[A]
+    def take(n: Int): MyStream[A] = this match {
+      case Empty => MyStream.empty[A]
       case Cons(h, t) =>
         if (n > 0)
           Cons(h, () => t().take(n - 1))
         else
-          StreamCustom.empty[A]
+          MyStream.empty[A]
     }
 
-    def drop(n: Int): StreamCustom[A] = this match {
-      case Empty => StreamCustom.empty[A]
+    def drop(n: Int): MyStream[A] = this match {
+      case Empty => MyStream.empty[A]
       case stream@Cons(h, t) =>
         if (n > 0)
           t().drop(n - 1)
         else
           stream
     }
+
+    def takeWhile(p: A => Boolean): MyStream[A] = this match {
+      case Empty => MyStream.empty[A]
+      case stream@Cons(h, t) =>
+        val a = h()
+        if (p(a)) Cons(h, () => t().takeWhile(p)) else MyStream.empty
+    }
+
+    def forAll(p: A => Boolean): Boolean = this match {
+      case Empty => true
+      case Cons(h, t) => p(h()) && t().forAll(p)
+    }
   }
 
-  case object Empty extends StreamCustom[Nothing]
-  case class Cons[+A](h: () => A, t: () => StreamCustom[A]) extends StreamCustom[A]
+  case object Empty extends MyStream[Nothing]
+  case class Cons[+A](h: () => A, t: () => MyStream[A]) extends MyStream[A]
 
-  object StreamCustom {
-    def cons[A](hd: => A, tl: => StreamCustom[A]): StreamCustom[A] = {
+  object MyStream {
+    def cons[A](hd: => A, tl: => MyStream[A]): MyStream[A] = {
       lazy val head = hd
       lazy val tail = tl
       Cons(() => head, () => tail)
     }
-    def empty[A]: StreamCustom[A] = Empty
-    def apply[A](as: A*): StreamCustom[A] =
+    def empty[A]: MyStream[A] = Empty
+    def apply[A](as: A*): MyStream[A] =
       if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
   }
 }
--- src/test/scala/fp/Chapter5Test.scala
+++ src/test/scala/fp/Chapter5Test.scala
@@ -5,17 +5,22 @@ import org.scalatest.FreeSpec
 class Chapter5Test extends FreeSpec {
   import Chapter5._
   "5.1" in {
-    assertResult(List(1, 2))(StreamCustom(1, 2).toList)
-    assertResult(Nil)(StreamCustom.empty.toList)
+    assertResult(List(1, 2))(MyStream(1, 2).toList)
+    assertResult(Nil)(MyStream.empty.toList)
   }
 
-  def infiniteStreamFrom(n: Int): StreamCustom[Int] = StreamCustom.cons(n, infiniteStreamFrom(n + 1))
+  def infiniteStreamFrom(n: Int): MyStream[Int] = MyStream.cons(n, infiniteStreamFrom(n + 1))
 
   "5.2" in {
     assertResult(List(1, 2))(infiniteStreamFrom(1).take(2).toList)
   }
 
   "5.3" in {
-    assertResult(List(2, 3))(infiniteStreamFrom(0).drop(2).take(2).toList)
+    assertResult(List(0, 1))(infiniteStreamFrom(0).takeWhile(_ < 2).toList)
+  }
+
+  "5.4" in {
+    assert(infiniteStreamFrom(0).takeWhile(_ < 2).forAll(_ < 2))
+    assert(!infiniteStreamFrom(0).takeWhile(_ < 2).forAll(_ < 1))
   }
 }
